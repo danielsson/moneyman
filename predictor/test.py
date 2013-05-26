f9268f0025702e14dc5955f2d39a8987bcbd31c5
Mattias Danielsson <mattias_d@hotmail.com>
2013-05-26 15:36:32 +0200

    Added travis

---
+++ predictor/test.py
@@ -0,0 +1,32 @@
+import unittest
+from predictor import TransactionClassifier
+from os.path import isfile
+import os
+
+class PredictorTest(unittest.TestCase):
+	TEST_DATA = [{"tag":"BULLE", "amount":400}]
+	TEST_TYPE = [4]
+	TEST_PERS_FILE = "test.pkl"
+
+	def setUp(self):
+		self.clf = TransactionClassifier(self.TEST_PERS_FILE)
+		self.clf._init_clf()
+
+	def tearDown(self):
+		if(isfile(self.TEST_PERS_FILE)):
+			os.remove(self.TEST_PERS_FILE)
+
+	def test_fit(self):
+		self.clf.fit(self.TEST_DATA, self.TEST_TYPE)
+
+	def test_predict(self):
+		self.test_fit()
+
+		self.assertEquals(self.TEST_TYPE, self.clf.predict(self.TEST_DATA))
+
+	def test_persist(self):
+		self.test_fit()
+
+		self.clf.persist()
+		
+		self.assertTrue(isfile(self.TEST_PERS_FILE))
